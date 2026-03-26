e15ab05a0f1c6911fb5c85b337f713fb38d8d08b
Anna <annamelnychuk264@gmail.com>
2026-03-26 22:06:45 +0200

    lab3 admin and models

--- app_blog/models.py
+++ app_blog/models.py
@@ -1,3 +1,64 @@
-from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    category = models.CharField('Категорія', max_length=250)
+    slug = models.SlugField('Слаг')
+
+    class Meta:
+        verbose_name = 'Категорія для публікації'
+        verbose_name_plural = 'Категорії для публікацій'
+
+    def __str__(self):
+        return self.category
+
+class Article(models.Model):
+    title = models.CharField('Заголовок', max_length=250)
+    description = models.TextField('Опис', blank=True)
+    pub_date = models.DateTimeField('Дата публікації', default=timezone.now)
+    slug = models.SlugField('Слаг', unique_for_date='pub_date')
+    main_page = models.BooleanField('Головна', default=False, help_text='Показувати на головній сторінці')
+
+    category = models.ForeignKey(
+    Category,
+    related_name='articles',
+    blank=True,
+    null=True,
+    on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Стаття'
+        verbose_name_plural = 'Статті'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/"
+
+
+class ArticleImage(models.Model):
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name='images'
+    )
+
+    image = models.ImageField('Фото', upload_to='photos')
+    title = models.CharField('Заголовок', max_length=250, blank=True)
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фото'
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def filename(self):
+        return self.image.name.split('/')[-1]
